9f6303f78ef71b7546ade90a81917b632e16f1aa
Maddock <143650411+okamidragon@users.noreply.github.com>
2024-10-25 09:24:21 -0500

    Added comments

--- Java-Lab3/src/MainApp.java
+++ Java-Lab3/src/MainApp.java
@@ -1,9 +1,14 @@
+// Maddock Davis
+// Lab 3: Main file
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
+        
+        // Loads the data from the CSV file
         SwingUtilities.invokeLater(() -> {
             DataLoader dataLoader = new DataLoader();
             List<DevelopmentIndicator> data = dataLoader.loadData("RenewableEnergy_Publications.csv");
@@ -14,18 +19,21 @@ public class MainApp {
                 return;
             }
 
+            // Creates the GUI
             JFrame frame = new JFrame("Data Visualization Application");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(new BorderLayout());
 
+            // Creates the table, statistics, and graph
             TablePanel tablePanel = new TablePanel(data);
             StatsPanel statsPanel = new StatsPanel(data);
             ChartPanel chartPanel = new ChartPanel(data);
 
+            // Formats the whole GUI
             frame.add(statsPanel, BorderLayout.NORTH);
             frame.add(tablePanel, BorderLayout.CENTER);
             frame.add(chartPanel, BorderLayout.SOUTH);
-
+            
             frame.setSize(800, 600);
             frame.setVisible(true);
         });
