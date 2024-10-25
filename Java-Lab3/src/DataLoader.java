3933e61656e093cb3e508aa4e15ac858fc484c19
Maddock <143650411+okamidragon@users.noreply.github.com>
2024-10-25 09:20:13 -0500

    Updated comments

--- Java-Lab3/src/DataLoader.java
+++ Java-Lab3/src/DataLoader.java
@@ -1,20 +1,26 @@
+// Maddock Davis
+// Lab 3: Data loader
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+// Loads the data from the CSV file
 public class DataLoader {
     public static List<DevelopmentIndicator> loadData(String filename) {
         List<DevelopmentIndicator> indicators = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
-            br.readLine(); // Skip header
+            br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(";");
                 String country = values[0];
                 List<Integer> publications = new ArrayList<>();
-                for (int i = 2; i < values.length; i++) { // Start from index 2 for the years
+
+                // Start from index 2 for the years
+                for (int i = 2; i < values.length; i++) {
                     publications.add(Integer.parseInt(values[i]));
                 }
                 indicators.add(new DevelopmentIndicator(country, publications));
